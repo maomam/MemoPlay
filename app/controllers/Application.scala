670177f7c49c5397d5ebdc5e3caab507174a75c8
Daryna <darynka@mail.ua>
2013-04-28 20:55:17 +0200

    Size change, theme change and reset

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -10,44 +10,35 @@ object Application extends Controller {
   var feld: Feld = new Feld(4, Theme.fruits)
   
   def index = Action {
-    Ok(views.html.index(4, Theme.people))
+    Redirect(routes.Application.showField("fruits",4))
   }
   
-   def redo (size: Int, theme : String) = Action {
-     var newTheme = Theme.people
-      theme match {
-      case "fruits" => newTheme = Theme.fruits
-      case "countries" => newTheme = Theme.countries
-      case "people" => newTheme = Theme.people
-      case "fashion" => newTheme = Theme.fashion
-
-    }
-    Ok(views.html.index(size, newTheme))
-  }
+ 
 
   var statusText = "Spiel angefangen"
 
-  def solve = Action {
-    feld.solve; statusText = "Spiel gelöst";
-    Ok(statusText)
-  }
+  
 
-  def changeTheme(stringTheme: String) = Action {
-    
+  def showField(stringTheme: String, newSize: Int) = Action {
     var newTheme :Theme.Value = Theme.fruits
     stringTheme match {
       case "fruits" => newTheme = Theme.fruits
       case "countries" => newTheme = Theme.countries
       case "people" => newTheme = Theme.people
       case "fashion" => newTheme = Theme.fashion
-
+      case _ => newTheme = currentTheme
     }
     if (feld.currentTheme != newTheme) {
       feld.setTheme(newTheme)
       statusText = "Thema der Bilder geändert"
-
     } else { statusText = "Das aktuelle Thema ist schon das geforderte" }
-    Ok("Theme (" + stringTheme +  ") was pressed")
+   
+    if (newSize != feld.dimension) {
+      feld.resize(newSize)
+      statusText = "Spielgrösse verändert"
+
+    } else { statusText = "Das Spiel ist schon in der geforderten grösse" }
+    Ok(views.html.index(newSize, stringTheme)(pictureBindingsJSON))
     
   }
 
@@ -69,19 +60,28 @@ object Application extends Controller {
 
   def reset = Action {
     feld.reset
-    statusText = "Zellen werden zurückgesetzt"
+    statusText = "Zellen werden zurueckgesetzt"
+    Redirect(routes.Application.showField(currentTheme.toString(), fieldSize))
+  }
+  
+  def solve = Action {
+    feld.solve;
+    statusText = "Spiel geloest";
+    //pictures
     Ok(statusText)
-
   }
 
-  def resize(newSize: Int) = Action {
-    if (newSize != feld.dimension) {
-      feld.resize(newSize)
-      statusText = "Spielgrösse verändert"
-
-    } else { statusText = "Das Spiel ist schon in der geforderten grösse" }
-     Ok("Size (" + newSize +  ") was pressed")
+  //TODO: make this lazy, not to recalculate it every time showField is called
+  def pictureBindingsJSON: String = {
+    val dimension = fieldSize
+    var result = "pictureBinding: {\n"
+    for (i <- 0 to dimension - 1; 
+      j <- 0 to dimension - 1) result += "  'c" + i + "," + j + "': " +  pictureNr((i,j)) + "\n"
+    result += "}\n"
+    result
   }
+  
+ 
 
   def pictureNr(c: Coordinates): Int = {
     feld(c._1, c._2).pictureNr
